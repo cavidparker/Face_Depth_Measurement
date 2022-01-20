aff7c3734243eba1073bfef0427505006fcf7859
cavidparker <cavidpark@gmail.com>
2022-01-20 17:29:24 +0600

    added draw distance

--- FaceDepthMeasurement.py
+++ FaceDepthMeasurement.py
@@ -23,16 +23,26 @@ while True:
         cv2.circle(img, nose, 5, (0, 255, 255), cv2.FILLED)
         w, _ = detector.findDistance(pointLeft, pointRight)
         # print(w)
+        W = 6.3
 
-        # Finding the focal length of the camera
-        W = 6.3  # cm for male person in front of camera
-        d = 50
-        f = (w*d)/W
-        if f > 600:
-            print("lost")
-        else:
-            print("good")
-        print(f)
+        # # Finding the focal length of the camera
+        # W = 6.3  # cm for male person in front of camera
+        # d = 50
+        # f = (w*d)/W
+        # if f > 600:
+        #     print("lost")
+        # else:
+        #     print("good")
+        # print(f)
+
+        ### Finding the distance ###
+        f = 840
+        d = (W*f)/w
+        print(d)
+
+        cvzone.putTextRect(img, f'Depth: {int(d)}cm',
+                           (face[10][0] - 100, face[10][1] - 50),
+                           scale=2)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
